6f410c230a9788131ed3c01d72245f7cc9fd2c3d
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2024-12-18 00:39:51 -0500

    - menu prematricula solo para admin - pagina prematricula solo con input por ahora

--- src/views/Dashboard/DashboardMenu.vue
+++ src/views/Dashboard/DashboardMenu.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 // data
 const navLinks = [
-  { routeName: 'cursos', text: 'Cursos' },
-  { routeName: 'matriculas', text: 'Matriculas' },
+  { routeName: 'cursos', text: 'Cursos', admin: false },
+  { routeName: 'matriculas', text: 'Matriculas', admin: false },
+  { routeName: 'prematricula', text: 'Pre-Matricula', admin: true },
 ]
+
+// store
+import { useAuthStore } from '@/stores/auth'
+const { perfil } = useAuthStore()
+
+// computed
+const esAdmin = computed(() => {
+  return perfil?.codigo_perfil_usuario === 1 || perfil?.codigo_perfil_usuario === 2
+})
 </script>
 
 <template>
   <nav class="flex items-baseline space-x-4 lg:space-x-6">
-    <RouterLink
-      v-for="{ routeName, text } in navLinks"
-      :key="routeName"
-      :to="{ name: routeName }"
-      class="text-sm font-medium transition-colors hover:text-primary"
-    >
-      {{ text }}
-    </RouterLink>
+    <template v-for="item in navLinks">
+      <RouterLink
+        v-if="!item.admin || (item.admin && esAdmin)"
+        :key="item.routeName"
+        :to="{ name: item.routeName }"
+        class="text-sm font-medium transition-colors hover:text-primary"
+      >
+        {{ item.text }}
+      </RouterLink>
+    </template>
   </nav>
 </template>
 
---
+++ src/views/Dashboard/Prematricula/PreMatriculaView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import Button from '@/components/ui/button/Button.vue'
+</script>
+
+<template>
+  <div class="flex-1 space-y-3 px-4 py-8 pt-3">
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <p>Pre matricula</p>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div class="grid w-full max-w-sm items-center gap-1.5">
+          <Label for="picture">Nomina (Archivo excel)</Label>
+          <div class="flex w-full max-w-sm items-center gap-1.5">
+            <Input id="picture" type="file" />
+            <Button> Procesar </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+</template>
+
+<style></style>
