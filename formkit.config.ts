0e2e5bc1ae2e1aefde7051238c317077ca54eb5f
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2024-12-27 13:10:27 -0500

    formkit

--- formkit.config.ts
+++ formkit.config.ts
@@ -8,42 +8,4 @@ export default defaultConfig({
   config: {
     rootClasses,
   },
-  plugins: [addAsteriskPlugin],
 })
-
-// plugins
-const legends = ['checkbox_multi', 'radio_multi', 'repeater', 'transferlist']
-function addAsteriskPlugin(node) {
-  if (['button', 'submit', 'hidden', 'group', 'list', 'meta'].includes(node.props.type)) return
-
-  node.on('created', () => {
-    const legendOrLabel = legends.includes(
-      `${node.props.type}${node.props.options ? '_multi' : ''}`,
-    )
-      ? 'legend'
-      : 'label'
-
-    if (node.props.definition.schemaMemoKey) {
-      node.props.definition.schemaMemoKey += `${node.props.options ? '_multi' : ''}_add_asterisk`
-    }
-
-    const schemaFn = node.props.definition.schema
-    node.props.definition.schema = (sectionsSchema = {}) => {
-      sectionsSchema[legendOrLabel] = {
-        children: [
-          '$label',
-          {
-            $el: 'span',
-            if: '$state.required',
-            attrs: {
-              class: '$classes.asterisk',
-            },
-            children: ['*'],
-          },
-        ],
-      }
-
-      return schemaFn(sectionsSchema)
-    }
-  })
-}
