275d5e819d50ad8f9e86ebc6284bd477a6350b4f
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2024-12-29 22:27:05 -0500

    menu dashboard mejorados

--- src/views/Dashboard/DashboardMenu.vue
+++ src/views/Dashboard/DashboardMenu.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
+// router
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
 // data
 const navLinks = [
   { routeName: 'cursos', text: 'Cursos', admin: false },
@@ -13,25 +17,38 @@ import { useAuthStore } from '@/stores/auth'
 const { perfil } = useAuthStore()
 
 // computed
-const esAdmin = computed(() => {
-  return perfil?.codigo_perfil_usuario === 1 || perfil?.codigo_perfil_usuario === 2
-})
+const esAdmin = computed(() => [1, 2].includes(perfil!.codigo_perfil_usuario)) // TODO (modelo) perfil usuario tiene q ser not null
+const menuUsuario = computed(() => navLinks.filter((item) => !item.admin || esAdmin))
+
+// methods
+const esUnLinkActivo = (routeName: string) => route.name === routeName
 </script>
 
 <template>
   <nav class="flex items-baseline space-x-4 lg:space-x-6">
-    <template v-for="item in navLinks">
-      <RouterLink
-        v-if="!item.admin || (item.admin && esAdmin)"
-        :key="item.routeName"
-        :to="{ name: item.routeName }"
-        class="text-sm font-medium transition-colors hover:text-primary"
-        :data-test="`nav-${item.routeName}`"
-      >
-        {{ item.text }}
-      </RouterLink>
+    <template v-for="item in menuUsuario" :key="item.routeName">
+      <template v-if="esUnLinkActivo(item.routeName)">
+        <p class="cursor-default text-sm font-extrabold">{{ item.text }}</p>
+      </template>
+      <template v-else>
+        <RouterLink
+          v-if="!item.admin || (item.admin && esAdmin)"
+          :key="item.routeName"
+          :to="{ name: item.routeName }"
+          :data-test="`nav-${item.routeName}`"
+        >
+          <p class="text-sm font-medium hover:text-red-600">{{ item.text }}</p>
+        </RouterLink>
+      </template>
     </template>
   </nav>
 </template>
 
 <style></style>
+
+<!-- :class="[
+            route.name === item.routeName
+              ? 'font-bold text-black'
+              : 'font-normal hover:text-red-600',
+          ]"
+          class="text-sm text-gray-600 transition-colors" -->
