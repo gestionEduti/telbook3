32e08835c1873f891938a9d7600cf4b520ee83b5
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2024-12-31 05:21:38 -0500

    test cursos

--- cypress/e2e/telbook/cursos.cy.js
+++ cypress/e2e/telbook/cursos.cy.js
@@ -3,9 +3,9 @@
 const interceptUrl = 'https://*.supabase.co/rest/v1/tp_cursos*'
 
 const niveles = [
-  { nombre: 'medio mayor', cursos: 3 },
-  { nombre: 'primer nivel de transicion', cursos: 2 },
-  { nombre: 'segundo nivel de transicion', cursos: 1 },
+  { sigla: 'MM', nombre: 'medio mayor', cursos: ['A', 'B', 'C'] },
+  { sigla: '1NT', nombre: 'primer nivel de transicion', cursos: ['A', 'B'] },
+  { sigla: '2NT', nombre: 'segundo nivel de transicion', cursos: ['A'] },
 ]
 
 describe('/cursos', () => {
@@ -17,55 +17,108 @@ describe('/cursos', () => {
   })
 
   it('deben haber 3 cursos del mismo nivel', () => {
+    const assertCursos = 3
+    const assertNiveles = 1
     cy.intercept('GET', interceptUrl, { fixture: 'cursos.json' }).as('getCursos')
     // se ven 1 nivel solamente
-    cy.getBySel('cursos-card-content').children().should('have.length', 1)
+    cy.getBySel('cursos-card-content').children().should('have.length', assertNiveles)
     // en cada nivel...
     cy.getBySel('cursos-card-content')
       .children()
-      .each(($child, index) => {
-        console.log(`nivel ${index} children ${$child}`)
+      .each(($nivel, nivelIndex) => {
+        const siglaNivel = niveles[nivelIndex].sigla // MM
+        const nombreNivel = niveles[nivelIndex].nombre // medio mayor
+        const cursosNivel = niveles[nivelIndex].cursos // ['A', 'B', 'C']
+        const cantidadCursosNivel = niveles[nivelIndex].cursos.length // 3
         // tiene el nombre que corresponde
-        cy.wrap($child).find('p').should('have.text', niveles[index].nombre)
+        cy.wrap($nivel).find('p').should('have.text', nombreNivel)
         // tiene la cantidad de cursos que corresponde
-        cy.wrap($child).find('li').children().should('have.length', niveles[index].cursos)
+        cy.wrap($nivel).find('li').children().should('have.length', cantidadCursosNivel)
+        cy.wrap($nivel)
+          .find('li')
+          .children()
+          .each(($curso, cursoIndex) => {
+            // tiene el link q corresponde
+            cy.wrap($curso).should(
+              'have.attr',
+              'href',
+              `/dashboard/cursos/${siglaNivel}/${cursosNivel[cursoIndex]}`,
+            )
+            cy.wrap($curso).should('have.text', cursosNivel[cursoIndex])
+          })
       })
     // dice que hay 3 cursos en total en el footer del Card
-    cy.getBySel('cursos-total-cursos').should('includes.text', '3 curso(s) en total')
+    cy.getBySel('cursos-total-cursos').should('includes.text', `${assertCursos} curso(s) en total`)
   })
 
-  it('deben haber 4 cursos de 2 niveles distintos', () => {
+  it('deben haber 5 cursos de 2 niveles distintos', () => {
+    const assertCursos = 5
+    const assertNiveles = 2
     cy.intercept('GET', interceptUrl, { fixture: 'cursos2.json' }).as('getCursos')
-    // se ven 2 niveles solamente
-    cy.getBySel('cursos-card-content').children().should('have.length', 2)
+    // se ven 1 nivel solamente
+    cy.getBySel('cursos-card-content').children().should('have.length', assertNiveles)
     // en cada nivel...
     cy.getBySel('cursos-card-content')
       .children()
-      .each(($child, index) => {
+      .each(($nivel, nivelIndex) => {
+        const siglaNivel = niveles[nivelIndex].sigla // MM
+        const nombreNivel = niveles[nivelIndex].nombre // medio mayor
+        const cursosNivel = niveles[nivelIndex].cursos // ['A', 'B', 'C']
+        const cantidadCursosNivel = niveles[nivelIndex].cursos.length // 3
         // tiene el nombre que corresponde
-        cy.wrap($child).find('p').should('have.text', niveles[index].nombre)
+        cy.wrap($nivel).find('p').should('have.text', nombreNivel)
         // tiene la cantidad de cursos que corresponde
-        cy.wrap($child).find('li').children().should('have.length', niveles[index].cursos)
-      })
-    // dice que hay 4 cursos en total en el footer del Card
-    cy.getBySel('cursos-total-cursos').should('includes.text', '5 curso(s) en total')
-  })
-
-  it('deben haber 5 cursos de 3 niveles distintos', () => {
-    cy.intercept('GET', interceptUrl, { fixture: 'cursos3.json' }).as('getCursos')
-    // se ven 2 niveles solamente
-    cy.getBySel('cursos-card-content').children().should('have.length', 3)
-    // en cada nivel...
-    cy.getBySel('cursos-card-content')
-      .children()
-      .each(($child, index) => {
-        // tiene el nombre que corresponde
-        cy.wrap($child).find('p').should('have.text', niveles[index].nombre)
-        // tiene la cantidad de cursos que corresponde
-        cy.wrap($child).find('li').children().should('have.length', niveles[index].cursos)
+        cy.wrap($nivel).find('li').children().should('have.length', cantidadCursosNivel)
+        cy.wrap($nivel)
+          .find('li')
+          .children()
+          .each(($curso, cursoIndex) => {
+            // tiene el link q corresponde
+            cy.wrap($curso).should(
+              'have.attr',
+              'href',
+              `/dashboard/cursos/${siglaNivel}/${cursosNivel[cursoIndex]}`,
+            )
+            cy.wrap($curso).should('have.text', cursosNivel[cursoIndex])
+          })
       })
     // dice que hay 5 cursos en total en el footer del Card
-    cy.getBySel('cursos-total-cursos').should('includes.text', '6 curso(s) en total')
+    cy.getBySel('cursos-total-cursos').should('includes.text', `${assertCursos} curso(s) en total`)
+  })
+
+  it('deben haber 6 cursos de 3 niveles distintos', () => {
+    const assertCursos = 6
+    const assertNiveles = 3
+    cy.intercept('GET', interceptUrl, { fixture: 'cursos3.json' }).as('getCursos')
+    // se ven 1 nivel solamente
+    cy.getBySel('cursos-card-content').children().should('have.length', assertNiveles)
+    // en cada nivel...
+    cy.getBySel('cursos-card-content')
+      .children()
+      .each(($nivel, nivelIndex) => {
+        const siglaNivel = niveles[nivelIndex].sigla // MM
+        const nombreNivel = niveles[nivelIndex].nombre // medio mayor
+        const cursosNivel = niveles[nivelIndex].cursos // ['A', 'B', 'C']
+        const cantidadCursosNivel = niveles[nivelIndex].cursos.length // 3
+        // tiene el nombre que corresponde
+        cy.wrap($nivel).find('p').should('have.text', nombreNivel)
+        // tiene la cantidad de cursos que corresponde
+        cy.wrap($nivel).find('li').children().should('have.length', cantidadCursosNivel)
+        cy.wrap($nivel)
+          .find('li')
+          .children()
+          .each(($curso, cursoIndex) => {
+            // tiene el link q corresponde
+            cy.wrap($curso).should(
+              'have.attr',
+              'href',
+              `/dashboard/cursos/${siglaNivel}/${cursosNivel[cursoIndex]}`,
+            )
+            cy.wrap($curso).should('have.text', cursosNivel[cursoIndex])
+          })
+      })
+    // dice que hay 6 cursos en total en el footer del Card
+    cy.getBySel('cursos-total-cursos').should('includes.text', `${assertCursos} curso(s) en total`)
   })
 
   it('debe decir que no hay cursos', () => {
