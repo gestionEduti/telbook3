a9b78756aec0cbefd4d71a4b473121fe41fbda33
jpuniacc <juan.silva@uniacc.cl>
2025-02-27 16:48:45 -0300

    se cambia el order de id por numero de matricula

--- src/stores/matriculas.ts
+++ src/stores/matriculas.ts
@@ -12,7 +12,7 @@ export const useMatriculasStore = defineStore('matriculas', () => {
       .select()
       .or('codigo_estado_alumno.eq.0,codigo_estado_alumno.eq.1')
       .eq('rbd_establecimiento', authStore.perfil!.rbd_usuario) // // TODO: setear error si es que el perfil no existe
-      .order('id', { ascending: true })
+      .order('numero_matricula_alumno', { ascending: true })
     if (error) errorStore.setError({ error: error, customCode: status })
     else alumnos.value = data
   }
